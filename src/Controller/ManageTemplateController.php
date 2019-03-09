474bca0c7a8f1c90084332bbb22f39267b055074
Arno Vuyk <arno@vuyk.eu>
2019-03-09 18:23:07 +0100

    including Twig

--- src/Controller/ManageTemplateController.php
+++ src/Controller/ManageTemplateController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ManageTemplateController
+class ManageTemplateController extends AbstractController
 {
     /**
      * @Route("/")
@@ -16,6 +17,8 @@ class ManageTemplateController
      * @Route("/admin", name="admin_home")
      */
     public function adminHome() {
-        return new Response('Admin homepage template gallery');
+        return $this->render('manage/adminHome.html.twig',[
+            'title'=>'Admin Home',
+        ]);
     }
 }
